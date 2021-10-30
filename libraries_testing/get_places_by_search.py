04f6343e627c5aac454677b873b148c2f032bc0a
GeneralCoder365 <ainesh.chatterjee@gmail.com>
2021-10-30 01:32:12 -0400

    algorithm part 2 done and working, need to implement in main, create composite of both algorithms and remove redundant functions

--- libraries_testing/get_places_by_search.py
+++ libraries_testing/get_places_by_search.py
@@ -25,19 +25,26 @@ def places_search(search_query: str) -> list:
     return formatted_search_results
 
 def at_address_risk_rating(formatted_address):
-    current_popularity = (livepopulartimes.get_populartimes_by_address(formatted_address))["current_popularity"]
+    place_data = livepopulartimes.get_populartimes_by_address(formatted_address)
+    current_popularity = place_data["current_popularity"]
     # current_popularity = None data type if no popularity data at current time
     
     if (current_popularity == None):
         current_day_of_week = dt.today().weekday()
-        current_hour = dt.now.hour()
+        current_hour = dt.now().hour
+        print(current_day_of_week)
+        print(current_hour)
 
-        # ADD GETTING TIME DIRECTLY FROM DAILY & HOURLY LIST
-        # if this doesn't give a value or is 0, account for that!
+        # today's popular times (0 AM - 11 PM so index = hour)
+        current_popularity = place_data["populartimes"][current_day_of_week - 1]["data"][current_hour]
 
+        # range: [0, 100) = (0 <= x < 101)
+        if (current_popularity not in range(0, 101)):
+            return None
+    
     return current_popularity
 
-print(places_search(input("Query: ")))
+# print(places_search(input("Query: ")))
 # at_address_risk_rating("(Westfield Montgomery) 7101 Democracy Blvd, Bethesda, MD 20852, United States")
-# print(at_address_risk_rating("(Starbucks) 10251 Old Georgetown Rd, Bethesda, MD 20814"))
+print(at_address_risk_rating("(Starbucks) 10251 Old Georgetown Rd, Bethesda, MD 20814"))
 # print(at_address_risk_rating("(McDonald's) 11564 Rockville Pike, Rockville, MD 20852"))
